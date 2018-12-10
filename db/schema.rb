9baec78999f01f3e9e558061528db5f9e8b4347a
kuroda-kentaro <kuroda_business@outlook.jp>
2018-12-10 09:40:23 +0900

    Create_ArticleTable_To_App

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_10_002555) do
+ActiveRecord::Schema.define(version: 2018_12_10_003614) do
+
+  create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.text "article"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", default: "", null: false
