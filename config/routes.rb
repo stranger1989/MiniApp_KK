3fce65121e84720c6f054cb86418768d05e7fcc9
kuroda-kentaro <kuroda_business@outlook.jp>
2018-12-10 09:48:25 +0900

    Create_Article_Path

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :articles, only: [:new,:show,:create,:destroy,:update]
+  root "articles#index"
 end
