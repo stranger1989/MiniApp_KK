e5c656444f22f0a347b754e267e12e15ed72d9d6
kuroda-kentaro <kuroda_business@outlook.jp>
2018-12-12 10:38:47 +0900

    Create_ArticleFunction

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,24 @@
 class ArticlesController < ApplicationController
 
   def index
+    @article = Article.order("id DESC").page(params[:page]).per(5)
   end
 
+  def new
+    @article = Article.new
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    Article.create(create_params)
+    redirect_to action: :index
+  end
+
+  private
+  def create_params
+    params.require(:article).permit(:title,:article).merge(user_id:current_user.id)
+  end
 end
