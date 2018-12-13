4dabd6369eba4c1746c00e44048e5264fd358c37
kuroda-kentaro <kuroda_business@outlook.jp>
2018-12-13 09:18:28 +0900

    Create_UpdateArticle_Function

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :destroy, :update, :edit]
 
   def index
     @article = Article.order("id DESC").page(params[:page]).per(5)
@@ -9,7 +10,6 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def create
@@ -17,8 +17,25 @@ class ArticlesController < ApplicationController
     redirect_to action: :index
   end
 
+  def destroy
+    @article.destroy if @article.user_id == current_user.id
+    redirect_to action: :index
+  end
+
+  def update
+    @article.update(create_params) if @article.user_id == current_user.id
+    redirect_to action: :index
+  end
+
+  def edit
+  end
+
   private
   def create_params
     params.require(:article).permit(:title,:article).merge(user_id:current_user.id)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
   end
 end
