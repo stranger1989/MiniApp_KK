0560048b58749cf19fa7680a028c141d83ffbff3
kuroda-kentaro <kuroda_business@outlook.jp>
2018-12-13 10:48:10 +0900

    Add_NavbarFunction

---
+++ app/assets/javascripts/app.js
@@ -0,0 +1,6 @@
+console.log("text");
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems, {});
+});
