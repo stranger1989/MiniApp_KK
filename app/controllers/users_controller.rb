15ba461afea5a50611c4818688dabb3dbf2ecf38
kuroda-kentaro <kuroda_business@outlook.jp>
2018-12-13 09:17:49 +0900

    Create_Mypage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,5 @@
 class UsersController < ApplicationController
+  def show
+    @article = Article.where(user_id:params[:id]).order("id DESC").page(params[:page]).per(5)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :articles, only: [:new,:show,:create,:destroy,:update]
+  resources :users, only: [:show]
   root "articles#index"
+  get 'articles/:id/edit' => 'articles#edit'
 end
